f69fa2722bcdc3ea7791ee8b5835efd4dfe92b54
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-03 10:39:56 -0600

    getting data and showing at component <JobInfo />

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import { JobInfo } from "./JobInfo";
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async() => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    // console.log(data)
+    setJobs(newJobs);
+    setIsLoading(false);
+  }
+
+  useEffect(()=> {
+    fetchJobs()
+  },[])
+
+  if(isLoading){
+    <section className="jobs-center">
+      <div className="loading"></div>
+    </section>
+  }
+
+  return (
+    <section className="jobs-center">
+      <JobInfo jobs={jobs}/>
+    </section>
+  );
 };
 export default App;
